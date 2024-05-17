4640c8564c1550f6ecaaa1db0994ded083dd03a4
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-05-17 21:50:57 +0300

    project management created

---
+++ project-management/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+
+
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleSelectProject(id) {
+    setProjectsState(PrevState => {
+      return {
+        ...PrevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+  function handleStartAddProject() {
+    setProjectsState(PrevState => {
+      return {
+        ...PrevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState(PrevState => {
+      const projectId = Math.random();
+      const newProject =  {
+        ...projectData,
+        id: projectId,
+      };
+      return {
+        ...PrevState,
+        selectedProjectId : undefined,
+        projects: [...PrevState.projects, newProject]
+      }
+    })
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState(PrevState => {
+      return {
+        ...PrevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  let content;
+  if(projectsState.selectedProjectId === null ) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
+
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+  return (
+    <main className="h-screen my-8 flex gap-8">
+     <ProjectSidebar onStartAddProject={handleStartAddProject} onSelectProject={handleSelectProject}
+      projects={projectsState.projects}
+       />
+     {content}
+    </main>
+  );
+}
+
+export default App;
