564e3dca1cff000e13955aa83d63e0fd8e5e9531
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-06-02 22:38:43 +0300

    useCallback Hook Created for the provide performans optimization with prevend useless rerender

--- useEffect-HandlingSideEffects/src/App.jsx
+++ useEffect-HandlingSideEffects/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -55,8 +55,7 @@ function App() {
 
   }
 
-
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -64,17 +63,20 @@ function App() {
 
     const storedIds = JSON.parse(localStorage.getItem("selectedPlaces"))  || [];
     localStorage.setItem("selectedPlaces",
-    JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current )))
+    JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current )));
 
-  }
+  }, []);
+  
 
   return (
     <>
-      <Modal open={modalIsOpen} onClose={handleStopRemovePlace} >
+      <Modal open={modalIsOpen} >
+        {/* {modalIsOpen && ( */}
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
         />
+      {/* // )} */}
       </Modal>
 
       <header>
